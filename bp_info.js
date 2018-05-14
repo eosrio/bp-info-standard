39072052940ec8497b7a8cf958dbd99f18292ffb
EOS Rio <38332581+eosrio@users.noreply.github.com>
2018-05-14 04:09:29 -0300

    initial revision

---
+++ bp_info.js
@@ -0,0 +1,20 @@
+const bp_info = {
+	candidate_name: String,
+	org_location: String,
+	org_country_code: String,
+	node_location: [{
+		place: String
+		latitude: Number,
+		longitude: Number
+	}],
+	website: String,
+	social: {
+		steemit: String,
+		twitter: String,
+		youtube: String,
+		facebook: String,
+		reddit: String,
+		keybase: String,
+		telegram: String
+	}
+}
