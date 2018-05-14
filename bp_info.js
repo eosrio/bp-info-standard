fe914aee3ca9e01de7d9c9088ff6901b80831eda
Jae Chung <jae.chung99@gmail.com>
2018-05-14 19:10:51 +0800

    Update bp_info.js

--- bp_info.js
+++ bp_info.js
@@ -1,9 +1,13 @@
 const bp_info = {
 	candidate_name: String,
+	is_producer: Boolean,
+	producer_account_name: String,
+	producer_public_key: string,
 	org_location: String,
 	org_country_code: String,
 	p2p_endpoint: String,
 	api_endpoint: String,
+	ssl_endpoint: String, //optional
 	node_location: [{
 		place: String
 		latitude: Number,
@@ -11,12 +15,12 @@ const bp_info = {
 	}],
 	website: String,
 	social: {
-		steemit: String,
-		twitter: String,
-		youtube: String,
-		facebook: String,
-		reddit: String,
-		keybase: String,
-		telegram: String
+		steemit: String, //optional
+		twitter: String, //optional
+		youtube: String, //optional
+		facebook: String, //optional
+		reddit: String, //optional
+		keybase: String, //optional
+		telegram: String //optional
 	}
 }
